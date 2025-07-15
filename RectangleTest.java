35e246c1e11494156f0baee6246b011442f4ea51
pevans123 <pevans12@charlotte.edu>
2025-07-15 14:08:22 -0400

    Update RectangleTest.java

--- RectangleTest.java
+++ RectangleTest.java
@@ -1,7 +1,13 @@
 import java.awt.Rectangle;
+import java.util.Scanner;
 
 public class RectangleTest {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter width: "); 
+        int width = input.nextInt();
+        System.out.print("Enter height: "); 
+        int height = input.nextInt();
         Rectangle box1 = new Rectangle(10, 10, 40, 30);
         System.out.println("box1: " + box1);
     }
